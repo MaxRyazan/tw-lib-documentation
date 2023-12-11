79245b50f7b1d1c2415cc8f35f36199a6ccea42c
Max <97405800+MaxRyazan@users.noreply.github.com>
2023-12-11 10:14:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tw-lib-docs/',
+  base: '/tw-lib-documentation/',
   plugins: [
     vue(),
   ],
