2ebefa1644f90c2ff53cbbf379950a1d7feb1ae7
MaxRyazan <no4ta87@mail.ru>
2023-12-16 23:19:41 +0300

    1.1.7  HomeComponent.vue адаптив, сделано

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import {shallowRef} from "vue";
 import TwTableBaseDoc from "@/documents/TwTableBaseDoc.vue";
 import HomeComponent from '@/components/HomeComponent.vue'
@@ -7,11 +7,24 @@ import TwDropdownDoc from "@/documents/TwDropdownDoc.vue";
 import TwInputDoc from "@/documents/TwInputDoc.vue";
 
 const currentComponent = shallowRef(HomeComponent)
+function toggleBurgerVisible(){
+    const burger: HTMLElement = document.querySelector('.nav')
+    if(burger) {
+        if(!burger.classList.contains('show')) burger.classList.add('show')
+        else burger.classList.remove('show')
+    }
+}
+
+function seeThis(clickedComponent:any){
+    currentComponent.value = clickedComponent
+    const burger: HTMLElement = document.querySelector('.nav')
+    if(burger) burger.classList.remove('show')
+}
 </script>
 
 <template>
     <div class="header">
-        <div class="burger_nav scss_green">Компоненты</div>
+        <div class="burger_nav scss_green" @click="toggleBurgerVisible"></div>
         <img src="./assets/img/logo.png" alt="logo">
         <a href="https://github.com/MaxRyazan/tw-lib-documentation" style="color: #3eaf7c;">github repo</a>
     </div>
@@ -19,11 +32,11 @@ const currentComponent = shallowRef(HomeComponent)
         <div class="nav">
             <p class="nav__title">Компоненты</p>
             <div class="nav__list">
-                <div @click="currentComponent=HomeComponent" class="nav__list_item" :class="{'tw_active': currentComponent === HomeComponent}">About</div>
-                <div @click="currentComponent=TwTableBaseDoc" class="nav__list_item" :class="{'tw_active': currentComponent === TwTableBaseDoc }">TwTableBase</div>
-                <div @click="currentComponent=TwButtonDoc" class="nav__list_item" :class="{'tw_active': currentComponent === TwButtonDoc}">TwButton</div>
-                <div @click="currentComponent=TwInputDoc" class="nav__list_item" :class="{'tw_active': currentComponent === TwInputDoc}">TwInput</div>
-                <div @click="currentComponent=TwDropdownDoc" class="nav__list_item" :class="{'tw_active': currentComponent === TwDropdownDoc}">TwDropdown</div>
+                <div @click="seeThis(HomeComponent)" class="nav__list_item" :class="{'tw_active': currentComponent === HomeComponent}">About</div>
+                <div @click="seeThis(TwTableBaseDoc)" class="nav__list_item" :class="{'tw_active': currentComponent === TwTableBaseDoc }">TwTableBase</div>
+                <div @click="seeThis(TwButtonDoc)" class="nav__list_item" :class="{'tw_active': currentComponent === TwButtonDoc}">TwButton</div>
+                <div @click="seeThis(TwInputDoc)" class="nav__list_item" :class="{'tw_active': currentComponent === TwInputDoc}">TwInput</div>
+                <div @click="seeThis(TwDropdownDoc)" class="nav__list_item" :class="{'tw_active': currentComponent === TwDropdownDoc}">TwDropdown</div>
             </div>
         </div>
         <div class="wrapper_main">
@@ -51,6 +64,10 @@ const currentComponent = shallowRef(HomeComponent)
     width: 120px;
     min-width: 120px;
     top: -2px;
+    display: none;
+    border-right: 2px solid $green;
+    border-bottom: 2px solid $green;
+    padding-bottom: 20px;
   }
   border-right: 2px solid $green;
   min-width: 200px;
@@ -77,6 +94,9 @@ const currentComponent = shallowRef(HomeComponent)
       display: inline-block;
       @media (max-width: 540px) {
         padding: 10px 5px;
+      }
+      @media (max-width: 800px) {
+        padding: 5px 5px;
       }
       &:hover {
         transition: .5s;
@@ -122,9 +142,34 @@ const currentComponent = shallowRef(HomeComponent)
   color: $green;
 }
 .burger_nav {
+  width: 40px;
+  height: 24px;
+  border-top: 2px solid white;
+  border-bottom: 2px solid white;
   display: none;
+  position: relative;
+  &:hover{
+    border-top: 2px solid orange;
+    border-bottom: 2px solid orange;
+    &:after{
+      background-color: orange;
+    }
+  }
+  &:after{
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+    height: 2px;
+    background-color: white;
+  }
   @media (max-width: 540px) {
     display: block;
   }
+}
+.show{
+  display: block !important;
 }
 </style>
