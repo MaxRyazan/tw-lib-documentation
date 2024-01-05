c16f1d80bc0cfa580aba98d1000c8e03a3eb960b
MaxRyazan <no4ta87@mail.ru>
2024-01-05 18:51:54 +0300

    1.3.6 tw-table-base DESC

--- src/documents/subcomponents/tw-table-balse-sub/TwTableBaseDescription.vue
+++ src/documents/subcomponents/tw-table-balse-sub/TwTableBaseDescription.vue
@@ -47,22 +47,29 @@ onMounted(() => {
                                    {a:'Balls weight', b:43, measure: 't.'},
                                    {a:'Water weight', b: 28, measure: 't.'}
                                ]"
-                             :row_custom_settings="[{idx: 0, isSubTitle: true, textColor: '#ffb83c'}, {idx: 4, isSubTitle: true, textColor: '#ffb83c'}]"
+                             :row_custom_settings="[
+                                 {idx: 0, isSubTitle: true, textColor: '#ffb83c'},
+                                 {idx: 4, isSubTitle: true, textColor: '#ffb83c'}
+                             ]"
             />
-            <tw-table-base v-else :header="['Parameter', 'Current value', 'Measure']"
+            <tw-table-base v-else
+                           :width="smallScreen ? '80%' : '700px'"
+                           :header="['Parameter', 'Current value', 'Measure']"
                            :body="[
-                                   {a:'Mechanical properties:', b: '', measure: ''},
+                                   {a:'Mechanical properties:'},
                                    {a:'Pressure', b:12, measure: 'atm.'},
                                    {a:'Temperature', b:-28, measure: 'f.'},
-                                   {a:'Danger index', b: 3, measure: 'lvl.'}
+                                   {a:'Danger index', b: 3, measure: 'lvl.'},
+                                   {a:'Calculating properties:'},
+                                   {a:'Flow', b:324.12, measure: 't.'},
+                                   {a:'Balls weight', b:43, measure: 't.'},
+                                   {a:'Water weight', b: 28, measure: 't.'}
                                ]"
-                           :width="smallScreen ? '80%' : '700px'"
-                           red_when_sub_zero
-                           header_font_color="green"
                            :row_custom_settings="[
-                                    {idx:0, textColor: 'darkorange', fontSize: '18px', bgc: 'rgb(255, 240, 219)', fontFamily: 'cursive'},
-                                ]"
-            ></tw-table-base>
+                                 {idx: 0, isSubTitle: true, textColor: '#ffb83c'},
+                                 {idx: 4, isSubTitle: true, textColor: '#ffb83c'}
+                             ]"
+            />
         </div>
         <div class="tag_wrapper">
             <template-tag>
@@ -71,7 +78,11 @@ onMounted(() => {
                         <span v-if="!smallScreen">width="700px"</span>
                         <span v-else>width="80%"</span>
                         <span>red_when_sub_zero</span>
-                        <span>header_font_color="green"</span>
+                        <span>header_bgc="#1a2b47"</span>
+                        <span>header_font_color="#b7d5d7"</span>
+                        <span>header_border_bottom="2px solid #00b6a6"</span>
+                        <span>table_border="1px solid #00b6a6"</span>
+                        <span>cell_font_color="#b7d5d7"</span>
                         <span style="display: flex">
                             :header="<span class="code__tag">header</span>"
                         </span>
@@ -86,21 +97,27 @@ onMounted(() => {
             </template-tag>
             <script-setup-tag>
                 <reactive-variable dataType="array" var-name="rowsSettings">
-                    <the-object-in-code v-if="!smallScreen" :object="{idx:0, textColor: 'darkorange', fontSize: '18px', fontFamily: 'cursive', bgc: 'rgb(255, 240, 219)'}" />
-                    <the-long-object-in-code v-else :object="{idx:0, textColor: 'darkorange', fontSize: '14px', fontFamily: 'cursive', bgc: 'rgb(255, 240, 219)'}" />
+                    <the-long-object-in-code v-if="smallScreen"  :object="{idx: 0, isSubTitle: true, textColor: '#ffb83c'}" />
+                    <the-long-object-in-code v-if="smallScreen"  :object="{idx: 4, isSubTitle: true, textColor: '#ffb83c'}" />
+                    <the-object-in-code v-if="!smallScreen" :object="{idx: 0, isSubTitle: true, textColor: '#ffb83c'}" />
+                    <the-object-in-code v-if="!smallScreen" :object="{idx: 4, isSubTitle: true, textColor: '#ffb83c'}" />
                 </reactive-variable>
                 <reactive-variable dataType="array" var-name="body">
-                    <the-object-in-code v-if="!smallScreen" :object="{a:'Mechanical properties:', b: '', measure: ''}" />
-                    <the-long-object-in-code v-else :object="{a:'Mech. props:', b: '', measure: ''}" />
-
+                    <the-long-object-in-code :object="{a:'Mechanical properties:'}" />
                     <the-object-in-code v-if="!smallScreen"  :object="{a:'Pressure', b:12, measure: 'atm.'}" />
                     <the-long-object-in-code v-else :object="{a:'Pressure', b:12, measure: 'atm.'}" />
-
                     <the-object-in-code v-if="!smallScreen"  :object="{a:'Temperature', b:1011, measure: 'f.'}" />
                     <the-long-object-in-code v-else :object="{a:'Temperature', b:1011, measure: 'f.'}" />
-
                     <the-object-in-code v-if="!smallScreen"  :object="{a:'Danger index', b: 3, measure: 'lvl.'}" />
                     <the-long-object-in-code v-else :object="{a:'Danger index', b: 3, measure: 'lvl.'}" />
+
+                    <the-long-object-in-code :object="{a:'Calculating properties:'}" />
+                    <the-object-in-code v-if="!smallScreen"  :object="{a:'Flow', b:324.12, measure: 't.'}" />
+                    <the-long-object-in-code v-else :object="{a:'Flow', b:324.12, measure: 't.'}" />
+                    <the-object-in-code v-if="!smallScreen"  :object="{a:'Balls weight', b:43, measure: 't.'}" />
+                    <the-long-object-in-code v-else :object="{a:'Balls weight', b:43, measure: 't.'}" />
+                    <the-object-in-code v-if="!smallScreen"  :object="{a:'Water weight', b: 28, measure: 't.'}" />
+                    <the-long-object-in-code v-else :object="{a:'Water weight', b: 28, measure: 't.'}" />
 
                 </reactive-variable>
                 <reactive-variable dataType="array" var-name="header">
